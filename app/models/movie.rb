df9af9ab07dd0db0618a2ce4d95bb7f312ca8572
ZhidaQu <zq2195@nyu.edu>
2026-03-02 11:07:33 -0500

    Finish ratings filter feature

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,12 @@
 class Movie < ApplicationRecord
+  def self.all_ratings
+    Movie.distinct.pluck(:rating).sort
+  end
+  def self.with_ratings(ratings_list)
+    if ratings_list.present?
+      Movie.where(rating: ratings_list)
+    else
+      Movie.all
+    end
+  end
 end
