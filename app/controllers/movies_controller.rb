e211561fac00b1d82205774c8be4c6455b2c832d
ZhidaQu <zq2195@nyu.edu>
2026-03-04 15:30:13 -0500

    Remember sorting and filtering settings using session

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,15 +4,28 @@ class MoviesController < ApplicationController
   # GET /movies or /movies.json
   def index
     @all_ratings = Movie.all_ratings
-    @ratings_to_show = params[:ratings]&.keys
-    @ratings_to_show = @all_ratings if @ratings_to_show.blank?
-    @movies = Movie.with_ratings(@ratings_to_show)
 
-
-    @sort_by = params[:sort_by]
-    if @sort_by.present? && ["title", "release_date"].include?(@sort_by)
-      @movies = @movies.order(@sort_by)
+    has_ratings_param = params.key?(:ratings)
+    has_sort_param    = params.key?(:sort_by)
+    if has_ratings_param
+      @ratings_to_show = params[:ratings]&.keys
+      @ratings_to_show = @all_ratings if @ratings_to_show.blank?
+    else
+      @ratings_to_show = session[:ratings_to_show]
+      @ratings_to_show = @all_ratings if @ratings_to_show.blank?
     end
+    allowed_sorts = ["title", "release_date"]
+    if has_sort_param
+      @sort_by = params[:sort_by]
+    else
+      @sort_by = session[:sort_by]
+    end
+    @sort_by = nil unless allowed_sorts.include?(@sort_by)
+    session[:ratings_to_show] = @ratings_to_show
+    session[:sort_by] = @sort_by
+
+    @movies = Movie.with_ratings(@ratings_to_show)
+    @movies = @movies.order(@sort_by) if @sort_by.present?
   end
 
   # GET /movies/1 or /movies/1.json
